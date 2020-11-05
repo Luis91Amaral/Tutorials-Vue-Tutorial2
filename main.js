2ed5fc4526c21c1a532a304fa058428a55ba1636
LAmaral_WavEC <48994042+luis91amaral@users.noreply.github.com>
2020-11-05 16:06:15 +0000

    Sixth lesson completed

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var shoppingList = new Vue({
   el: '#shopping-list',
   data: {
+    state: 'default',
     header: 'shopping list app',
     newItem: '',
     items: [
@@ -13,6 +14,10 @@ var shoppingList = new Vue({
     saveItem: function() {
       this.items.push(this.newItem)
       this.newItem = ''
+    },
+    changeState: function(newState) {
+      this.state = newState
+      newState = ''
     }
   }
 })
